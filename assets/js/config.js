ec46a65cae02f405287fa292f2c6b3a24a604f93
saberbalbouzi-ui <saber.balbouzi@gmail.com>
2026-09-24 17:27:57 +0100

    Update Apps Script API URL to new deployment (v2, 24 sept 2026)

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 /* ضع رابط Web App الخاص بك هنا (من Google Apps Script → Deploy → Web app → URL /exec) */
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbz48WhNa85YqjdeTILiwI66jKzsSP3t1XeS8PUOdJcRiEtaYEob7_sfAce18hloneOe/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbx8hfWsiB4UCn9u0Z7ySDZYVwvNXTAZMqoc-6SXFDi38m7EFGHr-eGPficx10gepj2C/exec",
   ADMIN_KEY: "ALYSSUM-ADMIN-2026",   // نفس المفتاح الموجود في Code.gs
 };
